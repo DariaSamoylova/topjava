6013b55f9c43fc26316c0bfd93b3d44c3b7068bf
DariaSamoylova <daria.borodina@gmail.com>
2019-02-25 16:29:01 +0300

    HW03 #2 done, creation table Meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_roles CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq
@@ -25,3 +26,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  user_id     INTEGER                 NOT NULL,
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  description VARCHAR                 NOT NULL,
+  dateTime    TIMESTAMP DEFAULT now() NOT NULL,
+  calories    INTEGER                 NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,6 +2,8 @@ DELETE
 FROM user_roles;
 DELETE
 FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq
   RESTART WITH 100000;
 
@@ -12,3 +14,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('ROLE_USER', 100000),
        ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, calories)
+VALUES (100000, 'Breakfast', 500),
+       (100000, 'Dinner', 500),
+       (100000, 'Supper', 500),
+       (100001, 'Breakfast', 500),
+       (100001, 'Dinner', 500),
+       (100001, 'Supper', 1800);
