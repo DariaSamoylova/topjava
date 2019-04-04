5dd1e2edc12d61df7836b65c83a3a0c73f7d4572
DariaSamoylova <daria.borodina@gmail.com>
2019-04-04 09:12:23 +0300

    HW07 #2 MealRestController getBetween fixed

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,8 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
 import java.net.URI;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -55,8 +57,16 @@ public class MealRestController extends AbstractMealController {
     }
 
 
-    @GetMapping("/between?startDate={datetime1}&endDate={datetime2}")
-    public List<MealTo> getBetween(@PathVariable LocalDateTime datetime1, @PathVariable LocalDateTime datetime2) {
-        return super.getBetween(datetime1.toLocalDate(), datetime1.toLocalTime(), datetime2.toLocalDate(), datetime2.toLocalTime());
+    //  @GetMapping("/between?startDate={datetime1}&endDate={datetime2}")
+//    public List<MealTo> getBetween(@PathVariable LocalDateTime datetime1, @PathVariable LocalDateTime datetime2) {
+//        return super.getBetween(datetime1.toLocalDate(), datetime1.toLocalTime(), datetime2.toLocalDate(), datetime2.toLocalTime());
+//    }
+
+    @GetMapping("/between?startDate={date1}&startTime={time1}&endDate={date2}&endTime={time2}")
+    public List<MealTo> getBetween(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date1, @PathVariable @DateTimeFormat(pattern = "hh:mm") LocalTime time1,
+                                   @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date2, @PathVariable @DateTimeFormat(pattern = "hh:mm") LocalTime time2) {
+        return super.getBetween(date1, time1, date2, time2);
+        //@DateTimeFormat(pattern="yyyy-MM-dd")  @DateTimeFormat(pattern="hh:mm")
+        //@DateTimeFormat(iso= DateTimeFormat.ISO.DATE)              @DateTimeFormat(iso= DateTimeFormat.ISO.TIME)
     }
 }
